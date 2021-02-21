15d826b5dde5d6bda83166b70602c9aca3d1bfe1
Jerry Li <jerryliuoft@gmail.com>
2021-02-21 13:49:42 -0500

    afixed card style on list page

--- src/app/collection-list/collection-list.component.ts
+++ src/app/collection-list/collection-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { List } from '../types';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-collection-list',
   templateUrl: './collection-list.component.html',
-  styleUrls: ['./collection-list.component.css']
+  styleUrls: ['./collection-list.component.css'],
 })
 export class CollectionListComponent implements OnInit {
+  public lists$: Observable<List[]>;
+  private store: AngularFirestore;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(store: AngularFirestore) {
+    this.lists$ = store.collection<List>('list').valueChanges();
+    this.store = store;
   }
 
+  ngOnInit(): void {}
 }
