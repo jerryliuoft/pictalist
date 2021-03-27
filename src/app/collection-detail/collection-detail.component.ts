0c8d20a649fe43fc0ab977e74ff455031b25b3c9
Jerry Li <jerryliuoft@gmail.com>
2021-03-26 22:04:03 -0400

    added meta tags for individual list page

--- src/app/collection-detail/collection-detail.component.ts
+++ src/app/collection-detail/collection-detail.component.ts
@@ -2,9 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Observable } from 'rxjs';
-import { switchMap, tap } from 'rxjs/operators';
+import { switchMap, take, tap } from 'rxjs/operators';
 import { AuthService } from '../services/auth.service';
 import { List } from '../types';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-collection-detail',
@@ -12,23 +13,41 @@ import { List } from '../types';
   styleUrls: ['./collection-detail.component.css'],
 })
 export class CollectionDetailComponent implements OnInit {
-  public list$: Observable<List | undefined>;
+  // public list$: Observable<List | undefined>;
+  public list: List | undefined;
 
   constructor(
     private route: ActivatedRoute,
-    store: AngularFirestore,
-    public user: AuthService
-  ) {
-    this.list$ = this.route.paramMap.pipe(
-      switchMap((params: ParamMap) =>
-        store
-          .doc<List>('lists/' + params.get('id'))
-          .valueChanges({ idField: 'id' })
-      )
-    );
-  }
+    private store: AngularFirestore,
+    public user: AuthService,
+    private title: Title,
+    private meta: Meta
+  ) {}
 
   ngOnInit(): void {
-    // this.items = [1, 2, 3, 4, 5, 6, 7, 8];
+    this.route.paramMap
+      .pipe(
+        switchMap((params: ParamMap) =>
+          this.store
+            .doc<List>('lists/' + params.get('id'))
+            .valueChanges({ idField: 'id' })
+        ),
+        take(1)
+      )
+      .subscribe((data) => {
+        this.list = data;
+        const title = 'Pictalist: ' + this.list?.title;
+        this.title.setTitle(title);
+        this.meta.addTags([
+          { name: 'twitter:card', content: 'summary' },
+          { name: 'og:url', content: 'https://pictalist/list/' + data?.id },
+          { name: 'og:title', content: title },
+          // { name: 'og:description', content: 'no Description' },
+          {
+            name: 'og:image',
+            content: this.list?.collection[0].thumbnailImage || '',
+          },
+        ]);
+      });
   }
 }
