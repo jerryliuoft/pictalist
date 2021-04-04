3adb2b604cbc2890c9a3f4da68bbd131240c79f6
Jerry Li <jerryliuoft@gmail.com>
2021-04-04 15:17:35 -0400

    added google search

--- src/app/item-search/shared/item-search.service.ts
+++ src/app/item-search/shared/item-search.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 import { response } from 'express';
 import { Item } from '../../types';
 import { Observable } from 'rxjs';
 import { UrlInfo } from '../../types';
+import { forkJoin } from 'rxjs';
 
 const WIKIPEDIA = {
   // Documentation is here https://www.mediawiki.org/wiki/API:Search#API_documentation
@@ -72,7 +73,17 @@ export class ItemSearchService {
           })
         );
     } else {
-      return this.getWikipedia(val);
+      return forkJoin(
+        // as of RxJS 6.5+ we can use a dictionary of sources
+        {
+          google: this.getGoogle(val),
+          wikipedia: this.getWikipedia(val),
+        }
+      ).pipe(
+        map((result) => {
+          return result.wikipedia.concat(result.google);
+        })
+      );
     }
   }
 
@@ -103,5 +114,44 @@ export class ItemSearchService {
         }
       })
     );
+  }
+
+  getGoogle(val: string): Observable<Item[]> {
+    return this.http
+      .get(
+        'https://us-central1-pictalist.cloudfunctions.net/searcher?search=' +
+          val
+      )
+      .pipe(
+        map((response: any) => {
+          const urlInfo: UrlInfo = response;
+          // the following are all trial and error not sure if it'll always work
+          // Here's an example response for searching final fantasy
+          // ")]}'\n[[[\"final fantasy 7 remake\",46,[433,275],{\"zh\":\"Final Fantasy VII Remake\",\"zi\":\"Video game\",\"zp\":{\"gs_ssp\":\"eJzj4tbP1TcwNE4zL8koM2D0EkvLzEvMUUhLzCtJLK5UMFcoSs1NzE4FANYSDAE\"},\"zs\":\"https://encrypted-tbn0.gstatic.com/images?q\\u003dtbn:ANd9GcSXDazKTiS47FGnAGPggjkU1vNKXhxE9FhfJRH_nD6eZXEHA5YPiSyMsgRvDw\\u0026s\\u003d10\"}],[\"final fantasy\",46,[433],{\"zh\":\"Final Fantasy\",\"zi\":\"Video game series\",\"zp\":{\"gs_ssp\":\"eJzj4tDP1TcwqiyvMGD04k3LzEvMUUhLzCtJLK4EAGMyCFA\"},\"zs\":\"https://encrypted-tbn0.gstatic.com/images?q\\u003dtbn:ANd9GcTvnsXkuHwK-tZHkEwKbxR60VgHuv9xEd4UTUtyVjL4BiWbib4sL3ABZ9L2Eg\\u0026s\\u003d10\"}],[\"final fantasy 14\",46,[],{\"zh\":\"FINAL FANTASY XIV Online\",\"zi\":\"Online game\",\"zp\":{\"gs_ssp\":\"eJzj4tLP1TcwM8nJM7A0YPQSSMvMS8xRSEvMK0ksrlQwNAEAg2MI7Q\"},\"zs\":\"https://encrypted-tbn0.gstatic.com/images?q\\u003dtbn:ANd9GcRty8AwIQj9cZNhFHxI2odID9oDp8nr6Y8dM61MZIA_XilzLdWjzxcaqpDUVQ\\u0026s\\u003d10\"}],[\"final fantasy 15\",46,[433,131],{\"zh\":\"FINAL FANTASY XV\",\"zi\":\"Video game\",\"zp\":{\"gs_ssp\":\"eJzj4tTP1TdIMbQsSDZg9BJIy8xLzFFIS8wrSSyuVDA0BQCAEQjh\"},\"zs\":\"https://encrypted-tbn0.gstatic.com/images?q\\u003dtbn:ANd9GcQ1vbNympADZOMmg9P8WdenG4aKuG1uMWN3FsNB3dpDrLEe6bQJ5q03mkH23A\\u0026s\\u003d10\"}],[\"final fantasy movie\",46,[433,131],{\"zh\":\"Final Fantasy: The Spirits Within\",\"zi\":\"2001 film\",\"zp\":{\"gs_ssp\":\"eJzj4tDP1Tcwii8sMWD0Ek7LzEvMUUhLzCtJLK5UyM0vy0wFAJkmCnI\"},\"zs\":\"https://encrypted-tbn0.gstatic.com/images?q\\u003dtbn:ANd9GcRc-dNe7DhOl3wLeVgStdeyNrP8DaiW5GqDRuHnGXHrRAe00lI2QOuLI_cKgQ\\u0026s\\u003d10\"}],[\"final fantasy 16\",46,[433],{\"zh\":\"Final Fantasy XVI\",\"zi\":\"Video game\",\"zp\":{\"gs_ssp\":\"eJzj4tVP1zc0zCg2sTDPMS0zYPQSSMvMS8xRSEvMK0ksrlQwNAMAo-AKBg\"},\"zs\":\"https://encrypted-tbn0.gstatic.com/images?q\\u003dtbn:ANd9GcQhx8PH_AX-RwynBNDPGKcpUGxz3fTY-PmWOwRAX8QtTbdMqvw803UeZL7isQ\\u0026s\\u003d10\"}],[\"final fantasy kingsglaive\",46,[433,131],{\"zh\":\"Kingsglaive: Final Fantasy XV\",\"zi\":\"2016 film\",\"zp\":{\"gs_ssp\":\"eJzj4tVP1zc0TM4rtqwqKcoxYPSSTMvMS8xRSEvMK0ksrlTIzsxLL07PScwsSwUALYgO8A\"},\"zs\":\"https://encrypted-tbn0.gstatic.com/images?q\\u003dtbn:ANd9GcQYx7xsLZT6WgGG1VTeywi_cYx0nBr5RjuOtDOUsI9hU6S6uM-hsi1w2fMXNQ\\u0026s\\u003d10\"}],[\"final fantasy 10\",46,[],{\"zh\":\"Final Fantasy X\",\"zi\":\"Video game\",\"zp\":{\"gs_ssp\":\"eJzj4tDP1TdIKTewMGD0EkjLzEvMUUhLzCtJLK5UMDQAAHZvCH0\"},\"zs\":\"https://encrypted-tbn0.gstatic.com/images?q\\u003dtbn:ANd9GcSrf6d05_Lj8UVy7Sy96VLi-Y64k37iVnDWAHqJsQBm\\u0026s\\u003d10\"}],[\"final fantasy tactics\",46,[433],{\"zh\":\"Final Fantasy Tactics\",\"zi\":\"Video game\",\"zp\":{\"gs_ssp\":\"eJzj4tDP1TdIKSswN2D0Ek3LzEvMUUhLzCtJLK5UKElMLslMLgYAsQYLSg\"},\"zs\":\"https://encrypted-tbn0.gstatic.com/images?q\\u003dtbn:ANd9GcQYOsq2sWkTleiWARrEdfBcdh5nyP4ry8_EVcBxGbUV\\u0026s\\u003d10\"}],[\"final fantasy\\u003cb\\u003e 11\\u003c\\/b\\u003e\",0,[433]]],{\"q\":\"lJsKyGwOZ73BxDg9tSK1Zu1xPc0\"}]"
+
+          const actualData = JSON.parse(response.split('\n')[1])[0];
+
+          const cards: Item[] = [];
+
+          actualData.forEach((data: any[], index: number) => {
+            if (data.length != 4) {
+              return;
+            } else {
+              const card: Item = {
+                id: index,
+                name: data[3].zh,
+                thumbnailImage: data[3].zs || '',
+                highResImage: data[3].zs || '',
+                description: data[3].zi || '',
+                url: 'https://www.google.ca/search?q=' + data[3].zh,
+                source: 'google',
+              };
+              cards.push(card);
+            }
+          });
+
+          return cards;
+        })
+      );
   }
 }
